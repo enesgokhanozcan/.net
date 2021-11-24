aad0f27b3ba8bb3fbd9fad8498677ea45d518709
Antropozen <enes.gokhan.ozcan@gmail.com>
2021-11-24 08:26:35 +0300

    get,put,post and delete endpoints added.

--- WebApi/Controllers/BookController.cs
+++ WebApi/Controllers/BookController.cs
@@ -44,5 +44,37 @@ namespace WebApi.AddCntrollers
             var book = BookList.Where(book=>book.Id==id).SingleOrDefault();
             return book;
         }
+        [HttpPost]
+        public  IActionResult AddBook([FromBody] Book newBook)
+        {
+            var book =BookList.SingleOrDefault(x=>x.Title == newBook.Title);
+
+            if(book is not null)
+                return BadRequest();
+            BookList.Add(newBook);
+            return Ok();
+        }
+        [HttpPut("{id}")]
+        public IActionResult UpdateBook(int id,[FromBody] Book updatedBook)
+        {
+            var book = BookList.SingleOrDefault(x=>x.Id == id);
+            if(book is null)
+                return BadRequest();
+            book.GenreId = updatedBook.GenreId != default ?  updatedBook.GenreId : book.GenreId;
+            book.PageCount = updatedBook.PageCount != default ?  updatedBook.PageCount : book.PageCount;
+            book.PublisDate = updatedBook.PublisDate != default ?  updatedBook.PublisDate : book.PublisDate;
+            book.Title = updatedBook.Title != default ?  updatedBook.Title : book.Title;
+            return Ok();
+        }
+        [HttpDelete("{id}")]
+        public IActionResult DeleteBook(int id)
+        {
+            var book = BookList.SingleOrDefault(x=>x.Id == id);
+            if(book is null)
+                return BadRequest();
+            BookList.Remove(book);
+            return Ok();
+        }
+
     }
 }
